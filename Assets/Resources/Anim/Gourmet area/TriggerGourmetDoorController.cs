dcac978b653d22324fc9ab94d40384c28528177e
Gabriel Aguido Fraga <gabriel_aguido@hotmail.com>
2022-08-03 22:05:58 -0400

    added anims

---
+++ Assets/Resources/Anim/Gourmet area/TriggerGourmetDoorController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TriggerGourmetDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator door = null;
+    [SerializeField] private bool doorTrigger = false;
+    private GameObject actionButton = null;
+
+    void Start() {
+        Transform[] trs = GameObject.Find("JoystickCanvas").GetComponentsInChildren<Transform>(true);
+        foreach(Transform t in trs) {
+            if(t.name == "UI_Virtual_Button_Action") {
+                actionButton = t.gameObject;
+                break;
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.CompareTag("Player")) {
+            actionButton.SetActive(true);
+
+            PointerEventData pointer = new PointerEventData(EventSystem.current);
+            if (Input.touchCount > 0) {
+                pointer.position = Input.GetTouch(0).position;
+                List<RaycastResult> raycastResults = new List<RaycastResult>();
+                EventSystem.current.RaycastAll(pointer, raycastResults);
+             
+                foreach (var go in raycastResults){
+                    if (go.gameObject.name == "UI_Virtual_Button_Action") {
+                        if (door.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1) {
+                            if(doorTrigger) {
+                                door.Play("OpenGourmetDoor", 0, 0.0f);
+                            } else {
+                                door.Play("CloseGourmetDoor", 0, 0.0f);
+                            }
+                        }
+
+                        doorTrigger = !doorTrigger;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        actionButton.SetActive(false);
+    }
+}
---
+++ Assets/Resources/Anim/Little bedroom/ToggleLittleBedroomLights.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ToggleLittleBedroomLights : MonoBehaviour
+{
+    private GameObject actionButton = null;
+
+    void Start() {
+        Transform[] trs = GameObject.Find("JoystickCanvas").GetComponentsInChildren<Transform>(true);
+        foreach(Transform t in trs) {
+            if(t.name == "UI_Virtual_Button_Action") {
+                actionButton = t.gameObject;
+                break;
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.CompareTag("Player")) {
+            actionButton.SetActive(true);
+
+            PointerEventData pointer = new PointerEventData(EventSystem.current);
+            if (Input.touchCount > 0) {
+                pointer.position = Input.GetTouch(0).position;
+                List<RaycastResult> raycastResults = new List<RaycastResult>();
+                EventSystem.current.RaycastAll(pointer, raycastResults);
+             
+                foreach (var go in raycastResults){
+                    if (go.gameObject.name == "UI_Virtual_Button_Action") {
+                        GameObject piece = GameObject.Find("Little bedroom");
+                        foreach (Transform transform in piece.transform)
+                        {
+                            if (transform.name == "Lights") {
+                                foreach(Transform light in transform)
+                                {
+                                    if(light.GetComponent<Light>().enabled == true)
+                                    {
+                                        light.GetComponent<Light>().enabled = false;
+                                    } else {
+                                        light.GetComponent<Light>().enabled = true;
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        actionButton.SetActive(false);
+    }
+}
