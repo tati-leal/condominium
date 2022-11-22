bd81b30cdcdbb07ddeff9c0b7be36ef379d96b3b
Gabriel Aguido Fraga <gabriel_aguido@hotmail.com>
2022-11-22 09:35:26 -0500

    testing a new way of building asset bundle

--- Assets/Resources/Scripts/Editor/CustomUnityCli.cs
+++ Assets/Resources/Scripts/Editor/CustomUnityCli.cs
@@ -41,7 +41,14 @@ public class CustomUnityCli : MonoBehaviour
 
         //Build iOS asset bundles
         Debug.Log("Building iOS asset bundle: " + arg);
-        BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", BuildAssetBundleOptions.None, BuildTarget.iOS);
+
+        AssetBundleBuild[] build = new AssetBundleBuild[1];
+        build[0] = new AssetBundleBuild();
+        build[0].assetBundleName = "name";
+        build[0].assetNames = new string[1] { asset_path };
+        BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", build, BuildAssetBundleOptions.None, BuildTarget.iOS);
+
+        //BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", BuildAssetBundleOptions.None, BuildTarget.iOS);
         Debug.Log("iOS asset bundle built: " + arg);
     }
 
